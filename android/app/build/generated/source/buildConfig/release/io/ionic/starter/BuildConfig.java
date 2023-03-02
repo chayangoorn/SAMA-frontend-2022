94096c7647af06f3c7ff13cb82e486eddf1adfb1
Paetong <108146560+chayangoorn@users.noreply.github.com>
2023-03-02 16:08:06 +0700

    update 2023

---
+++ android/app/build/generated/source/buildConfig/release/io/ionic/starter/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.ionic.starter;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.pccp.activitys";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 15;
+  public static final String VERSION_NAME = "2.0.0";
+}
